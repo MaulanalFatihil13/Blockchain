f63b1a6f87c78f83891d625e28275c5d1cae2960
MaulanalFatihil13 <78371193+MaulanalFatihil13@users.noreply.github.com>
2023-10-10 13:18:46 +0700

    Final Last Update

--- blockchain.py
+++ blockchain.py
@@ -1,38 +1,53 @@
-# import libraries
-
 import datetime
 import hashlib
 import json
 from flask import Flask, jsonify, request
 
-# 1. Blockchain (Server)
 class Blockchain:
 
     def __init__(self):
         self.chain = []
-        # genesis block
-        self.create_block(proof=1, prev_hash='0')
+        self.create_genesis_block()
 
-    def create_block(self, proof, prev_hash, proof_of_work=None, data=None):
+    def create_genesis_block(self):
+        # Genesis block initialization
+        block = {
+            'id_block': 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': 1,
+            'prev_hash': '0',
+            'hash': self.calculate_hash(1, '0', 1, None, None),
+            'proof_of_work': None,
+            'data': None
+        }
+        self.chain.append(block)
+
+    def create_block(self, proof, data):
+        prev_block = self.get_last_block()
+        prev_proof = prev_block['proof']
+        prev_hash = prev_block['hash']
+
+        proof_of_work, curr_hash = self.proof_of_work(prev_proof, proof)
+
         block = {
             'id_block': len(self.chain) + 1,
             'timestamp': str(datetime.datetime.now()),
             'proof': proof,
             'prev_hash': prev_hash,
+            'hash': curr_hash,
             'proof_of_work': proof_of_work,
             'data': data
         }
-        block['hash'] = self.get_hash(block)
+
         self.chain.append(block)
         return block
 
     def get_last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, prev_proof):
-        new_proof = 1
+    def proof_of_work(self, prev_proof, new_proof):
         check_proof = False
-        while check_proof is False:
+        while not check_proof:
             hash_operation = hashlib.sha256(str(new_proof**2 - prev_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
@@ -40,46 +55,48 @@ class Blockchain:
                 new_proof += 1
         return new_proof, hash_operation
 
-    def get_hash(self, block):
-        hash_operation = hashlib.sha256(json.dumps(block).encode()).hexdigest()
-        return hash_operation
+    def calculate_hash(self, id_block, prev_hash, proof, proof_of_work, data):
+        block_data = {
+            'id_block': id_block,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'prev_hash': prev_hash,
+            'proof_of_work': proof_of_work,
+            'data': data
+        }
+        return hashlib.sha256(json.dumps(block_data).encode()).hexdigest()
 
     def is_chain_valid(self):
         prev_block = self.chain[0]
-        now_index = 1
-        while now_index < len(self.chain):
-            now_block = self.chain[now_index]
+        current_index = 1
 
-            # 1. cek apakah hash now block = prev hash 
-            prev_hash = self.get_hash(prev_block)
-            if prev_hash != now_block['prev_hash']:
+        while current_index < len(self.chain):
+            current_block = self.chain[current_index]
+
+            if current_block['prev_hash'] != self.calculate_hash(
+                    current_block['id_block'] - 1,
+                    prev_block['hash'],
+                    current_block['proof'],
+                    current_block['proof_of_work'],
+                    current_block['data']
+            ):
                 return False
 
-            # 2. cek apakah proof of worknya true
-            now_proof = now_block['proof']
             prev_proof = prev_block['proof']
-            hash_operation = hashlib.sha256(str(now_proof**2 - prev_proof**2).encode()).hexdigest()
+            current_proof = current_block['proof']
+            hash_operation = hashlib.sha256(str(current_proof**2 - prev_proof**2).encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
 
-            prev_block = now_block
-            now_index += 1
+            prev_block = current_block
+            current_index += 1
+
         return True
 
-    def modify_block(self, block_index, new_data):
-        if block_index < len(self.chain):
-            modified_block = self.chain[block_index]
-            modified_block['data'] = new_data
-            modified_block['hash'] = self.get_hash(modified_block)
-            return True
-        return False
 
-# 2. Web Aplikasi untuk Testing
 app = Flask(__name__)
-
 blockchain = Blockchain()
 
-# Buat endpoint
 @app.route("/get_chain", methods=['GET'])
 def get_chain():
     response = {
@@ -88,57 +105,60 @@ def get_chain():
     }
     return jsonify(response), 200
 
-@app.route("/mining", methods=['GET'])
-def mining():
+@app.route("/mine", methods=['GET'])
+def mine():
     prev_block = blockchain.get_last_block()
-    # get prev proof
     prev_proof = prev_block['proof']
-    proof, proof_of_work = blockchain.proof_of_work(prev_proof)
-    # get prev hash
-    prev_hash = blockchain.get_hash(prev_block)
-    # prev_hash = prev_block['hash']
-    # create block
-    created_block = blockchain.create_block(proof, prev_hash, proof_of_work)
+
+    # Proof of work
+    new_proof = 1
+    proof_of_work, curr_hash = blockchain.proof_of_work(prev_proof, new_proof)
+
+    # Create a new block with some data (in this case, "Mined Block")
+    block_data = "Mined Block"
+    new_block = blockchain.create_block(proof_of_work, block_data)
+
     response = {
-        'message': "Blockchain is successfully created",
-        'created block': created_block
+        'message': "New block has been mined!",
+        'block': new_block
     }
     return jsonify(response), 200
 
-@app.route("/create", methods=['POST'])
-def create_block():
-    data = request.form
-
-    prev_block = blockchain.get_last_block()
-    # get prev proof
-    prev_proof = prev_block['proof']
-    proof, proof_of_work = blockchain.proof_of_work(prev_proof)
-    # get prev hash
-    prev_hash = blockchain.get_hash(prev_block)
-    # create block
-    created_block = blockchain.create_block(proof, prev_hash, proof_of_work, data['data'])
-    response = {
-        'message': "Blockchain is successfully created",
-        'created block': created_block
-    }
-    return jsonify(response), 200
-
-@app.route("/modify_block", methods=['POST'])
-def modify_block():
-    data = request.form
-    block_index = int(data['block_index'])
-    new_data = data['new_data']
-
-    if blockchain.modify_block(block_index, new_data):
-        response = {
-            'message': f"Block {block_index} has been modified",
-            'new_block_data': new_data
-        }
-        return jsonify(response), 200
+@app.route("/modify_block/<int:block_id>", methods=['POST'])
+def modify_block(block_id):
+    data = request.form.get('data')
+    
+    if data is not None:
+        if blockchain.modify_block(block_id, data):
+            response = {
+                'message': f"Block {block_id} has been modified",
+                'new_block_data': data
+            }
+            return jsonify(response), 200
+        else:
+            response = {
+                'message': f"Block {block_id} not found or cannot be modified"
+            }
+            return jsonify(response), 404
     else:
         response = {
-            'message': f"Block {block_index} not found or cannot be modified"
+            'message': "Please provide data to modify the block"
         }
-        return jsonify(response), 404
+        return jsonify(response), 400
 
-app.run()
+def modify_block(self, block_id, new_data):
+    if 0 < block_id <= len(self.chain):
+        modified_block = self.chain[block_id - 1]  # Index adjustment
+        modified_block['data'] = new_data
+        modified_block['hash'] = self.calculate_hash(
+            modified_block['id_block'],
+            modified_block['prev_hash'],
+            modified_block['proof'],
+            modified_block['proof_of_work'],
+            modified_block['data']
+        )
+        return True
+    return False
+
+if __name__ == "__main__":
+    app.run(debug=True)
